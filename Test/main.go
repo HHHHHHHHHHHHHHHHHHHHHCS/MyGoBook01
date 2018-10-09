99e9a0a4d02f0cf98dd285504a0fae0791192ffa
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-09 19:48:51 +0800

    未完成

--- Test/main.go
+++ Test/main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"sync"
 	"time"
 )
 
+var bytePool = sync.Pool{
+	New: func() interface{} {
+		b := make([]byte, 1024)
+		return &b
+	},
+}
+
+func main() {
+	t := time.Now()
+
+	for i := 0; i < 1E10; i++ {
+		obj := make([]byte, 1024)
+		_ = obj
+	}
+
+	elapsed1 := time.Since(t)
+	t = time.Now()
+
+	for i := 0; i < 1E10; i++ {
+		obj := bytePool.Get().(*[]byte)
+		_ = obj
+		bytePool.Put(obj)
+	}
+
+	elapsed2 := time.Since(t)
+
+	fmt.Println("A:", elapsed1)
+	fmt.Println("B:", elapsed2)
+}
