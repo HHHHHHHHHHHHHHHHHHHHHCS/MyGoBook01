34524511d6211738a08e019386d92182409a897a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-11 17:40:51 +0800

    json

---
+++ OtherLib/MyJson.go
@@ -0,0 +1,123 @@
+package OtherLib
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type (
+	GResult struct {
+		GsearchResultClass string `json:"GsearchResultClass"`
+		UnescapedURL       string `json:"unescapedUrl"`
+		URL                string `json:"url"`
+		VisibleURL         string `json:'visibleUrl'`
+		CacheURL           string `json:"cacheUrl"`
+		Title              string `json:"title"`
+		TitleNoFormatting  string `json:"titleNoFormatting"`
+		Content            string `json:"content"`
+	}
+
+	GResponse struct {
+		ResponseData struct {
+			Results []GResult `json:"results"`
+		} `json:"responseData"`
+	}
+)
+
+func MyJsonMain() {
+	Test02()
+}
+
+func Test01() {
+	url := "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=golang"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer resp.Body.Close()
+
+	var gr GResponse
+	err = json.NewDecoder(resp.Body).Decode(&gr)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+	fmt.Println(gr)
+}
+
+func Test02() {
+	var jsonString = `{
+	"responseData": {
+		"results": [{
+				"GsearchResultClass": "GwebSearch",
+				"unescapedUrl": "https://www.reddit.com/r/golang",
+				"url": "https://www.reddit.com/r/golang",
+				"visibleUrl": "www.reddit.com",
+				"cacheUrl": "http://www.google.com/search?q=cache:W...",
+				"title": "r/<b>Golang</b> - Reddit",
+				"titleNoFormatting": "r/Golang - Reddit",
+				"content": "First Open Source <b>Golang%u..."
+			},
+			{
+				"GsearchResultClass": "GwebSearch",
+				"unescapedUrl": "http://tour.golang.org/",
+				"url": "http://tour.golang.org/",
+				"visibleUrl": "tour.golang.org",
+				"cacheUrl": "http://www.google.com/search?q=cache:O...",
+				"title": "A Tour of Go",
+				"titleNoFormatting": "A Tour of Go",
+				"content": "Welcome to a tour of the Go programming ..."
+			}
+		]
+	}
+}`
+
+	var gr GResponse
+	json.Unmarshal([]byte(jsonString), &gr)
+	fmt.Println(gr)
+}
+
+func Test03() {
+	type T struct {
+		A int
+		B string
+		C byte
+		D struct {
+			D_A int
+			D_B string
+		}
+		E bool
+	}
+
+	t := &T{
+		A: 1,
+		B: "Test b",
+		C: 255,
+		D: struct {
+			D_A int
+			D_B string
+		}{
+			D_A: 123123,
+			D_B: "asdasd",
+		},
+		E: true,
+	}
+
+	data, _ := json.Marshal(t)
+	fmt.Println(string(data))
+}
+
+func Test04() {
+	type T struct {
+		Aa string `json:"aa"`
+	}
+
+	var t T
+
+	str := `{"aa":"a123a"}`
+	json.Unmarshal([]byte(str), &t)
+	fmt.Println(t)
+}
--- Test/main.go
+++ Test/main.go
@@ -1,61 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"time"
-	"../OtherLib"
-)
+import "../OtherLib"
 
-const length =1024
-
-func Test01() {
-	t := time.Now()
-
-	var arr [length * length * length]int
-
-	for i := 0; i < length*length*length; i++ {
-		arr[i] *= arr[i]
-		arr[i] *= arr[i]
-		arr[i] *= arr[i]
-		arr[i] *= arr[i]
-		arr[i] *= arr[i]
-	}
-
-	elapsed1 := time.Since(t)
-	fmt.Println("A:", elapsed1)
-}
-
-func Test02() {
-	t := time.Now()
-
-	var arr [length][length][length]int
-
-	for i := 0; i < length; i++ {
-		for j := 0; j < length; j++ {
-			for k := 0; k < length; k++ {
-				arr[i][j][k] *= arr[i][j][k]
-				arr[i][j][k] *= arr[i][j][k]
-				arr[i][j][k] *= arr[i][j][k]
-				arr[i][j][k] *= arr[i][j][k]
-				arr[i][j][k] *= arr[i][j][k]
-			}
-		}
-	}
-
-	elapsed1 := time.Since(t)
-	fmt.Println("B:", elapsed1)
-
-}
-
-func main() {
-	OtherLib.WorkPoolTest()
-
-
-	return
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	go Test01()
-
-
-	time.Sleep(100*time.Second)
+func main()  {
+	OtherLib.MyJsonMain()
 }
